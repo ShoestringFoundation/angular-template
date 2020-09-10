b04ebc5e915cfc78a443362a668a8c514d998262
Chikai Ohazama <chikai.ohazama@gmail.com>
2020-09-10 14:19:45 -0700

    Add angularfire configuration to angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireFunctionsModule, ORIGIN } from '@angular/fire/functions';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +16,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireFunctionsModule,
+    AngularFireStorageModule,
   ],
-  providers: [],
+  providers: [
+    { provide: ORIGIN, useValue: `${window.location.protocol}//${window.location.host}` }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
